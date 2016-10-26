2566c85118a6f6eb6311373d60ddd656ee86a2e7
Tony Perrin <tony.perrin@yahoo.co.uk>
2016-10-26 15:38:24 +0100

    Adding API methods for jar with tests.

--- src/main/java/com/example/api/CoinChange.java
+++ src/main/java/com/example/api/CoinChange.java
@@ -1,6 +1,8 @@
 package com.example.api;
 
 import com.example.model.Coin;
+import com.example.properties.PropertyManager;
+import com.example.repositories.CoinRepository;
 import com.example.services.CoinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,15 @@ import java.util.Collection;
  */
 @Component
 public class CoinChange {
-    @Autowired
-    private CoinService coinService;
+    private static final CoinService coinService;
+    private static final CoinRepository coinRepository;
+    private static final PropertyManager propertyManager;
 
+    static {
+        propertyManager = new PropertyManager();
+        coinRepository = new CoinRepository(propertyManager);
+        coinService = new CoinService(coinRepository);
+    }
 
     public Collection<Coin> getOptimalChangeFor(int amount) {
         return coinService.getOptimalChangeFor(amount);
